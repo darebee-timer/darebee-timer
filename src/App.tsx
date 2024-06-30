13c1f78452d6b93456a0327b7e817f7ea26601f0
Florian Richter <florian@richter-es.de>
2024-06-30 15:17:55 +0200

    Add bootstrap and main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Darebee Timer App </a>
+        </div>
+      </nav>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <div className="card-body">
+        <h5 className="card-title">New workout</h5>
+        <p className="card-text">Create new workaout schedule here.</p>
+        <a href="#create" className="btn btn-primary">Create Workout</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    </div>
+    <div className="card" style={{width: '18rem'}}>
+      <div className="card-body">
+        <h5 className="card-title">Last workout</h5>
+        <p className="card-text">20-20-20 20-20-20 20-20-20 7 sets</p>
+        <a href="#create" className="btn btn-primary">Edit</a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
-  )
+
+  );
 }
 
 export default App
