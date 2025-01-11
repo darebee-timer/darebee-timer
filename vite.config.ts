d21dfc8e98046ed8e2ef5a9d603eb6d04cb1694e
Florian Richter <florian@richter-es.de>
2025-01-11 14:25:36 +0000

    Provide PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 import path from 'path'
 
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ['favicon.ico'],
+  manifest: {
+    name: "Darebee Timer",
+    short_name: "Darebee Timer",
+    description: "A simple timer app for interval workouts",
+    icons: [{
+      src: '/android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+      purpose: 'favicon'
+    },
+    {
+      src: '/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes: '180x180',
+      type: 'image/png',
+      purpose: 'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes: '512x512',
+      type: 'image/png',
+      purpose: 'any maskable',
+    }
+    ],
+    theme_color: '#ffffff',
+    background_color: '#99c1f1',
+    display: "standalone",
+    scope: '/',
+    start_url: "/",
+    orientation: 'portrait'
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +51,5 @@ export default defineConfig({
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     }
   },
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 })
